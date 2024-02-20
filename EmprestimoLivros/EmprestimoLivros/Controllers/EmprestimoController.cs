2ad22afd67c134ee743a66c8c9187ec3bc06a1f5
Raul Henrique Furtado <raulfurtado8quali@outlook.com>
2024-02-20 13:31:09 -0300

    aula1 e perte da aula2

---
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,60 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public EmprestimoController(ApplicationDbContext db)
+        {
+              _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+
+            return View(emprestimos);
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id is null) return NotFound();
+
+            var emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo is null) return NotFound();
+
+            return View(emprestimo);
+        }
+
+        //[HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel request)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(request);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult EditarLivro(EmprestimosModel request)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(request);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ EmprestimoLivros/EmprestimoLivros/Migrations/20240220142910_CorrecaoNome.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmprestimoLivros.Migrations
+{
+    public partial class CorrecaoNome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Fornacedor",
+                table: "Emprestimos",
+                newName: "Fornecedor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Fornecedor",
+                table: "Emprestimos",
+                newName: "Fornacedor");
+        }
+    }
+}
